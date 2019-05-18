9ceda0f8b64e03065a9c671b5f92bfd7ea8b04dd
Barış Köklü <bariskoklu@gmail.com>
2019-05-18 17:32:13 +0300

    ortaya bir adet button eklendi. Evler arası geçişi sağlamak için

---
+++ borabet/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public List<GameObject> buildings;
+    public int currentBuildingNumber = 0;
+    public GameObject currentBuilding;
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, currentBuilding.transform.position.z + 7);
+    }
+    public void ChangeBuilding()
+    {
+
+        currentBuildingNumber++;
+        if (currentBuildingNumber >= 3)
+        {
+            currentBuildingNumber = 0;
+        }
+
+        currentBuilding = buildings[currentBuildingNumber];
+        this.ChangeCamera(currentBuilding);
+    }
+
+    private void ChangeCamera(GameObject currentBuilding)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, currentBuilding.transform.position.z + 7);
+    }
+}
---
+++ borabet/Assets/Scripts/TouchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log(touch.position);
+            }
+        }
+    }
+}
