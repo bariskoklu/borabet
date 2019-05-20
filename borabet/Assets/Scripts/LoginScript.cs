d79b05e031e6217d698aa2d3d61ed1521a3bfeec
Barış Köklü <bariskoklu@gmail.com>
2019-05-20 20:14:00 +0300

    Login ve register ui. Buttona tıklayınca login veya register infoyu bir listeye atıyor.

---
+++ borabet/Assets/Scripts/LoginScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginScript : MonoBehaviour
+{
+    public GameObject usernameObject;
+    public GameObject passwordObject;
+
+    private string username;
+    private string password;
+    private List<string> LoginInformation = new List<string>();
+
+    public void LoginButton()
+    {
+        username = usernameObject.GetComponent<InputField>().text;
+        password = passwordObject.GetComponent<InputField>().text;
+
+        LoginInformation.Add(username);
+        LoginInformation.Add(password);
+    }
+}
---
+++ borabet/Assets/Scripts/RegisterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterScript : MonoBehaviour
+{
+    public GameObject usernameObject;
+    public GameObject passwordObject;
+    public GameObject confPasswordObject;
+    public GameObject emailObject;
+
+    private string username;
+    private string password;
+    private string confPassword;
+    private string email;
+    private List<string> registerInformation = new List<string>();
+
+    public void RegisterButton()
+    {
+        if (string.Equals(passwordObject.GetComponent<InputField>().text, confPasswordObject.GetComponent<InputField>().text))
+        {
+            username = usernameObject.GetComponent<InputField>().text;
+            password = passwordObject.GetComponent<InputField>().text;
+            confPassword = confPasswordObject.GetComponent<InputField>().text;
+            email = emailObject.GetComponent<InputField>().text;
+
+            registerInformation.Add(username);
+            registerInformation.Add(password);
+            registerInformation.Add(confPassword);
+            registerInformation.Add(email);
+        }
+    }
+}
